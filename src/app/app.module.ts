72145e148992d8896614081f54723c868c90b286
cb <csbutton01@hotmail.com>
2018-06-18 16:53:01 -0700

    set up components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoutingComponent } from './routing/routing.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/routing/routing.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [ ];
+
+@Component({
+  selector: 'app-routing',
+  templateUrl: './routing.component.html',
+  styleUrls: ['./routing.component.scss']
+})
+export class RoutingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
