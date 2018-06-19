91ec49a792d32fc45fa4ac0c3dbc00dedd8c1416
cb <csbutton01@hotmail.com>
2018-06-18 18:00:18 -0700

    implement sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RoutingComponent } from './routing/routing.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { LocationsComponent } from './locations/locations.component';
+import { NewsComponent } from './news/news.component';
+import { OffersComponent } from './offers/offers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoutingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    NavMenuComponent,
+    LocationsComponent,
+    NewsComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule
--- src/app/routing/routing.component.ts
+++ src/app/routing/routing.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from '../welcome/welcome.component';
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  }
+ ];
+
+ export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
 @Component({
   selector: 'app-routing',
